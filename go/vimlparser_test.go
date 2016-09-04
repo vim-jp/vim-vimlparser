182eee51c9a19d9617662b1c39fd53ce5c73b2de
haya14busa <hayabusa1419@gmail.com>
2016-09-05 04:36:38 +0900

    go: add test for StringReader.__init__

--- go/vimlparser_test.go
+++ go/vimlparser_test.go
@@ -2,6 +2,7 @@ package vimlparser
 
 import (
 	"runtime/debug"
+	"strings"
 	"testing"
 )
 
@@ -11,5 +12,34 @@ func TestNewStringReader(t *testing.T) {
 			t.Errorf("Recovered: %v\n%s", r, debug.Stack())
 		}
 	}()
-	NewStringReader([]string{})
+	r := NewStringReader([]string{})
+	if !r.eof() {
+		t.Error("NewStringReader should call __init__ func to initialize")
+	}
+}
+
+func TestStringReader___init__(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("Recovered: %v\n%s", r, debug.Stack())
+		}
+	}()
+	tests := []struct {
+		in  []string
+		buf string
+	}{
+		{in: []string{}, buf: ""},
+		{in: []string{""}, buf: "<EOL>"},
+		{in: []string{"let x = 1"}, buf: "let x = 1<EOL>"},
+		{in: []string{"let x = 1", "let y = x"}, buf: "let x = 1<EOL>let y = x<EOL>"},
+		{in: []string{"let x =", `\ 1`}, buf: "let x = 1<EOL>"},
+		{in: []string{"あいうえお"}, buf: "あいうえお<EOL>"},
+	}
+	for _, tt := range tests {
+		r := &StringReader{}
+		r.__init__(tt.in)
+		if got := strings.Join(r.buf, ""); got != tt.buf {
+			t.Errorf("StringReader.__init__(%v).buf == %v, want %v", tt.in, got, tt.buf)
+		}
+	}
 }
