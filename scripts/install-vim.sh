526a765da451ee7f77e6452a11218c239ad5e8f2
haya14busa <haya14busa@gmail.com>
2017-11-24 12:17:54 +0000

    install huge version for profile command

--- scripts/install-vim.sh
+++ scripts/install-vim.sh
@@ -4,6 +4,6 @@ set -ev
 
 git clone --depth 1 --branch "${VIM_VERSION}" https://github.com/vim/vim /tmp/vim
 cd /tmp/vim
-./configure --prefix="${HOME}/vim" --with-features=big --enable-fail-if-missing
+./configure --prefix="${HOME}/vim" --with-features=huge --enable-fail-if-missing
 make -j2
 make install
