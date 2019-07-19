b378fd4e2365c5b76d08182fc4234816b93962cd
Daniel Hahler <git@thequod.de>
2019-07-19 16:13:48 +0200

    autopep8

--- py/vimlfunc.py
+++ py/vimlfunc.py
@@ -5,6 +5,7 @@ import sys
 import re
 import inspect
 
+
 def main():
     use_neovim = sys.argv[1] == '--neovim'
 
@@ -18,96 +19,108 @@ def main():
         print(e)
         sys.exit(1)
 
+
 class VimLParserException(Exception):
     pass
+
 
 class AttributeDict(dict):
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
 
+
 pat_vim2py = {
-  "[0-9a-zA-Z]" : "[0-9a-zA-Z]",
-  "[@*!=><&~#]" : "[@*!=><&~#]",
-  "\\<ARGOPT\\>" : "\\bARGOPT\\b",
-  "\\<BANG\\>" : "\\bBANG\\b",
-  "\\<EDITCMD\\>" : "\\bEDITCMD\\b",
-  "\\<NOTRLCOM\\>" : "\\bNOTRLCOM\\b",
-  "\\<TRLBAR\\>" : "\\bTRLBAR\\b",
-  "\\<USECTRLV\\>" : "\\bUSECTRLV\\b",
-  "\\<USERCMD\\>" : "\\bUSERCMD\\b",
-  "\\<\\(XFILE\\|FILES\\|FILE1\\)\\>" : "\\b(XFILE|FILES|FILE1)\\b",
-  "\\S" : "\\S",
-  "\\a" : "[A-Za-z]",
-  "\\d" : "\\d",
-  "\\h" : "[A-Za-z_]",
-  "\\s" : "\\s",
-  "\\v^d%[elete][lp]$" : "^d(elete|elet|ele|el|e)[lp]$",
-  "\\v^s%(c[^sr][^i][^p]|g|i[^mlg]|I|r[^e])" : "^s(c[^sr][^i][^p]|g|i[^mlg]|I|r[^e])",
-  "\\w" : "[0-9A-Za-z_]",
-  "\\w\\|[:#]" : "[0-9A-Za-z_]|[:#]",
-  "\\x" : "[0-9A-Fa-f]",
-  "^++" : "^\+\+",
-  "^++bad=\\(keep\\|drop\\|.\\)\\>" : "^\\+\\+bad=(keep|drop|.)\\b",
-  "^++bad=drop" : "^\\+\\+bad=drop",
-  "^++bad=keep" : "^\\+\\+bad=keep",
-  "^++bin\\>" : "^\\+\\+bin\\b",
-  "^++edit\\>" : "^\\+\\+edit\\b",
-  "^++enc=\\S" : "^\\+\\+enc=\\S",
-  "^++encoding=\\S" : "^\\+\\+encoding=\\S",
-  "^++ff=\\(dos\\|unix\\|mac\\)\\>" : "^\\+\\+ff=(dos|unix|mac)\\b",
-  "^++fileformat=\\(dos\\|unix\\|mac\\)\\>" : "^\\+\\+fileformat=(dos|unix|mac)\\b",
-  "^++nobin\\>" : "^\\+\\+nobin\\b",
-  "^[A-Z]" : "^[A-Z]",
-  "^\\$\\w\\+" : "^\\$[0-9A-Za-z_]+",
-  "^\\(!\\|global\\|vglobal\\)$" : "^(!|global|vglobal)$",
-  "^\\(WHILE\\|FOR\\)$" : "^(WHILE|FOR)$",
-  "^\\(vimgrep\\|vimgrepadd\\|lvimgrep\\|lvimgrepadd\\)$" : "^(vimgrep|vimgrepadd|lvimgrep|lvimgrepadd)$",
-  "^\\d" : "^\\d",
-  "^\\h" : "^[A-Za-z_]",
-  "^\\s" : "^\\s",
-  "^\\s*\\\\" : "^\\s*\\\\",
-  "^[ \\t]$" : "^[ \\t]$",
-  "^[A-Za-z]$" : "^[A-Za-z]$",
-  "^[0-9A-Za-z]$" : "^[0-9A-Za-z]$",
-  "^[0-9]$" : "^[0-9]$",
-  "^[0-9A-Fa-f]$" : "^[0-9A-Fa-f]$",
-  "^[0-9A-Za-z_]$" : "^[0-9A-Za-z_]$",
-  "^[A-Za-z_]$" : "^[A-Za-z_]$",
-  "^[0-9A-Za-z_:#]$" : "^[0-9A-Za-z_:#]$",
-  "^[A-Za-z_][0-9A-Za-z_]*$" : "^[A-Za-z_][0-9A-Za-z_]*$",
-  "^[A-Z]$" : "^[A-Z]$",
-  "^[a-z]$" : "^[a-z]$",
-  "^[vgslabwt]:$\\|^\\([vgslabwt]:\\)\\?[A-Za-z_][0-9A-Za-z_#]*$" : "^[vgslabwt]:$|^([vgslabwt]:)?[A-Za-z_][0-9A-Za-z_#]*$",
-  "^[0-7]$" : "^[0-7]$",
-  "^[0-9A-Fa-f][0-9A-Fa-f]$" : "^[0-9A-Fa-f][0-9A-Fa-f]$",
-  "^\.[0-9A-Fa-f]$" : "^\.[0-9A-Fa-f]$",
-  "^[0-9A-Fa-f][^0-9A-Fa-f]$" : "^[0-9A-Fa-f][^0-9A-Fa-f]$",
+    "[0-9a-zA-Z]": "[0-9a-zA-Z]",
+    "[@*!=><&~#]": "[@*!=><&~#]",
+    "\\<ARGOPT\\>": "\\bARGOPT\\b",
+    "\\<BANG\\>": "\\bBANG\\b",
+    "\\<EDITCMD\\>": "\\bEDITCMD\\b",
+    "\\<NOTRLCOM\\>": "\\bNOTRLCOM\\b",
+    "\\<TRLBAR\\>": "\\bTRLBAR\\b",
+    "\\<USECTRLV\\>": "\\bUSECTRLV\\b",
+    "\\<USERCMD\\>": "\\bUSERCMD\\b",
+    "\\<\\(XFILE\\|FILES\\|FILE1\\)\\>": "\\b(XFILE|FILES|FILE1)\\b",
+    "\\S": "\\S",
+    "\\a": "[A-Za-z]",
+    "\\d": "\\d",
+    "\\h": "[A-Za-z_]",
+    "\\s": "\\s",
+    "\\v^d%[elete][lp]$": "^d(elete|elet|ele|el|e)[lp]$",
+    "\\v^s%(c[^sr][^i][^p]|g|i[^mlg]|I|r[^e])": "^s(c[^sr][^i][^p]|g|i[^mlg]|I|r[^e])",
+    "\\w": "[0-9A-Za-z_]",
+    "\\w\\|[:#]": "[0-9A-Za-z_]|[:#]",
+    "\\x": "[0-9A-Fa-f]",
+    "^++": "^\+\+",
+    "^++bad=\\(keep\\|drop\\|.\\)\\>": "^\\+\\+bad=(keep|drop|.)\\b",
+    "^++bad=drop": "^\\+\\+bad=drop",
+    "^++bad=keep": "^\\+\\+bad=keep",
+    "^++bin\\>": "^\\+\\+bin\\b",
+    "^++edit\\>": "^\\+\\+edit\\b",
+    "^++enc=\\S": "^\\+\\+enc=\\S",
+    "^++encoding=\\S": "^\\+\\+encoding=\\S",
+    "^++ff=\\(dos\\|unix\\|mac\\)\\>": "^\\+\\+ff=(dos|unix|mac)\\b",
+    "^++fileformat=\\(dos\\|unix\\|mac\\)\\>": "^\\+\\+fileformat=(dos|unix|mac)\\b",
+    "^++nobin\\>": "^\\+\\+nobin\\b",
+    "^[A-Z]": "^[A-Z]",
+    "^\\$\\w\\+": "^\\$[0-9A-Za-z_]+",
+    "^\\(!\\|global\\|vglobal\\)$": "^(!|global|vglobal)$",
+    "^\\(WHILE\\|FOR\\)$": "^(WHILE|FOR)$",
+    "^\\(vimgrep\\|vimgrepadd\\|lvimgrep\\|lvimgrepadd\\)$": "^(vimgrep|vimgrepadd|lvimgrep|lvimgrepadd)$",
+    "^\\d": "^\\d",
+    "^\\h": "^[A-Za-z_]",
+    "^\\s": "^\\s",
+    "^\\s*\\\\": "^\\s*\\\\",
+    "^[ \\t]$": "^[ \\t]$",
+    "^[A-Za-z]$": "^[A-Za-z]$",
+    "^[0-9A-Za-z]$": "^[0-9A-Za-z]$",
+    "^[0-9]$": "^[0-9]$",
+    "^[0-9A-Fa-f]$": "^[0-9A-Fa-f]$",
+    "^[0-9A-Za-z_]$": "^[0-9A-Za-z_]$",
+    "^[A-Za-z_]$": "^[A-Za-z_]$",
+    "^[0-9A-Za-z_:#]$": "^[0-9A-Za-z_:#]$",
+    "^[A-Za-z_][0-9A-Za-z_]*$": "^[A-Za-z_][0-9A-Za-z_]*$",
+    "^[A-Z]$": "^[A-Z]$",
+    "^[a-z]$": "^[a-z]$",
+    "^[vgslabwt]:$\\|^\\([vgslabwt]:\\)\\?[A-Za-z_][0-9A-Za-z_#]*$": "^[vgslabwt]:$|^([vgslabwt]:)?[A-Za-z_][0-9A-Za-z_#]*$",
+    "^[0-7]$": "^[0-7]$",
+    "^[0-9A-Fa-f][0-9A-Fa-f]$": "^[0-9A-Fa-f][0-9A-Fa-f]$",
+    "^\.[0-9A-Fa-f]$": "^\.[0-9A-Fa-f]$",
+    "^[0-9A-Fa-f][^0-9A-Fa-f]$": "^[0-9A-Fa-f][^0-9A-Fa-f]$",
 }
+
 
 def viml_add(lst, item):
     lst.append(item)
 
+
 def viml_call(func, *args):
     func(*args)
+
 
 def viml_char2nr(c):
     return ord(c)
 
+
 def viml_empty(obj):
     return len(obj) == 0
+
 
 def viml_equalci(a, b):
     return a.lower() == b.lower()
 
+
 def viml_eqreg(s, reg):
     return re.search(pat_vim2py[reg], s, re.IGNORECASE)
+
 
 def viml_eqregh(s, reg):
     return re.search(pat_vim2py[reg], s)
 
+
 def viml_eqregq(s, reg):
     return re.search(pat_vim2py[reg], s, re.IGNORECASE)
+
 
 def viml_escape(s, chars):
     r = ''
@@ -118,17 +131,22 @@ def viml_escape(s, chars):
             r += c
     return r
 
+
 def viml_extend(obj, item):
     obj.extend(item)
 
-def viml_insert(lst, item, idx = 0):
+
+def viml_insert(lst, item, idx=0):
     lst.insert(idx, item)
+
 
 def viml_join(lst, sep):
     return sep.join(lst)
 
+
 def viml_keys(obj):
     return obj.keys()
+
 
 def viml_len(obj):
     if type(obj) is str:
@@ -139,17 +157,20 @@ def viml_len(obj):
         return len(b)
     return len(obj)
 
+
 def viml_printf(*args):
     if len(args) == 1:
         return args[0]
     else:
         return args[0] % args[1:]
 
+
 def viml_range(start, end=None):
     if end is None:
         return range(start)
     else:
         return range(start, end + 1)
+
 
 def viml_readfile(path):
     lines = []
@@ -159,23 +180,28 @@ def viml_readfile(path):
     f.close()
     return lines
 
+
 def viml_remove(lst, idx):
     del lst[idx]
+
 
 def viml_split(s, sep):
     if sep == "\\zs":
         return s
     raise VimLParserException("NotImplemented")
 
+
 def viml_str2nr(s, base=10):
     return int(s, base)
+
 
 def viml_string(obj):
     return str(obj)
 
+
 def viml_has_key(obj, key):
     return key in obj
 
+
 def viml_stridx(a, b):
     return a.find(b)
-
