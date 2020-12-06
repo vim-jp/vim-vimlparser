3e8ed5afedb11ef0f05a5a217a0f274bb120e4a1
itchyny <itchyny@hatena.ne.jp>
2020-12-06 10:38:17 +0900

    Disable default vimrc and viminfo in compile scripts

--- scripts/jscompile.sh
+++ scripts/jscompile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-vim -E -s -N --cmd 'set rtp+=.' -c 'exe "so" argv()[0]' -c q -- js/jscompiler.vim $*
+vim -u NONE -i NONE -E -s -N --cmd 'set rtp+=.' -c 'exe "so" argv()[0]' -c q -- js/jscompiler.vim $*
--- scripts/pycompile.sh
+++ scripts/pycompile.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-vim -E -s -N --cmd 'set rtp+=.' -c 'exe "so" argv()[0]' -c q -- py/pycompiler.vim $*
+vim -u NONE -i NONE -E -s -N --cmd 'set rtp+=.' -c 'exe "so" argv()[0]' -c q -- py/pycompiler.vim $*
